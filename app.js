57c595786f2b0630f096526740c6244699d367ce
Kate <annar91@onet.pl>
2018-01-03 20:15:00 +0000

    update dburl

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var commentRoutes	 = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	indexRoutes		 = require("./routes/index");
 
-// mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://kate:haslo1@ds135917.mlab.com:35917/yelp-camp");
+
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://kate:haslo1@ds135917.mlab.com:35917/yelp-camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
