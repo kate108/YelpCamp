b4e5088cb2635a90bc867e3af4f32abb48b77dd9
Kate <annar91@onet.pl>
2018-01-03 19:24:39 +0000

    initial deploy commit

---
+++ seeds.js
@@ -0,0 +1,60 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+
+var data = [
+	{
+		name: "Clouds Rest",
+		image: "https://media-cdn.tripadvisor.com/media/photo-s/05/c6/94/80/red-squirrel-campsite.jpg",
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	},
+	{
+		name: "Desert Mesa",
+		image: "https://www.nationaltrust.org.uk/images/1431734709816-hafodyllan6.jpg",
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	},
+	{
+		name: "Canyon Floor",
+		image: "https://static1.squarespace.com/static/57a33100579fb3f47b0e4f5f/57c33c569f745643297c9a2b/57c33c568419c2d24d5e3839/1472412848948/Campsite7.jpg",
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	},
+];
+
+function seedDB(){
+	/// Remove all campgrounds
+	Campground.remove({}, function(err){
+		// if(err){
+		// 	console.log(err);
+		// }
+		// console.log("removed campgrounds!");
+		// /// Add a few campgrounds
+		// data.forEach(function(seed){
+		// 	Campground.create(seed, function(err, campground){
+		// 		if(err){
+		// 			console.log(err);
+		// 		} else {
+		// 			console.log("added a campground");
+		// 			/// create a comment
+		// 			Comment.create(
+		// 				{
+		// 					text: "This place is great, but I wish there was Internet",
+		// 					author: "Homer"
+		// 				}, function(err, comment){
+		// 					if(err){
+		// 						console.log(err);
+		// 					} else {
+		// 						campground.comments.push(comment);
+		// 						campground.save();
+		// 						console.log("Created new comment");
+		// 					}
+		// 				});
+		// 		}
+		// 	});
+		// });
+	});
+
+	
+	/// Add a few comments
+}
+
+module.exports = seedDB;
